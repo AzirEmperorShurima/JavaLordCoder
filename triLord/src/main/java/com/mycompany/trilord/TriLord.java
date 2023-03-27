e7c0ca633338f96bf5c625a959406f3fefa4feb8
VanThen60hz <nguyenvthang2409@gmail.com>
2023-03-27 12:30:27 +0700

    them moi

---
+++ triLord/src/main/java/com/mycompany/trilord/TriLord.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trilord;
+
+/**
+ *
+ * @author Văn Thén
+ */
+public class TriLord {
+
+    public static void main(String[] args) {
+        System.out.println("Tri Ngu!");
+    }
+}
